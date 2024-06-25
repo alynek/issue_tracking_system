9b2accacfc09677ad10be06e577a54a046644663
alynek <alyne0x67@gmail.com>
2024-06-25 18:57:36 -0300

    Try deploy to github pages

--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -7,6 +7,7 @@ import {IssuesService} from '../issues.service'
   templateUrl: './issue-list.component.html',
   styleUrls: ['./issue-list.component.css']
 })
+
 export class IssueListComponent implements OnInit{
   issues: Issue[] = []
   showReportIssue = false;

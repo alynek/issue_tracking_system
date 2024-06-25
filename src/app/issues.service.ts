f065d506d3562e4da084394154d61cf8d13498cc
alynek <alyne0x67@gmail.com>
2024-06-24 21:37:20 -0300

    Add issue service

--- src/app/issues.service.ts
+++ src/app/issues.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+import {Issue} from './issue'
 
 @Injectable({
   providedIn: 'root'
 })
 export class IssuesService {
+  private issues: Issue[] = []
 
   constructor() { }
+
+  getPendingIssues(): Issue[]{
+    return this.issues.filter(_ => !_.completed)
+  }
 }
